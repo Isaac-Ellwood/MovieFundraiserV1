0927b2d5f7ba02859ff2dad297051f0d6e008c15
Isaac Ellwood <isaellw@chbc.school.nz>
2023-02-23 12:43:49 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieFundraiserV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> snacksAvailable = new List<string>() {"popcorn","corn","chips"};
+            List<float> snackPrices = new List<float>() { 1f, 2f, 1.5f};
+            
+            List<string> drinksAvailable = new List<string>() {"coke","cocoa cola","refined cola"};
+            List<float> drinkPrices = new List<float>() { 100f, 2f, 2.5f};
+
+            Console.WriteLine("Hello World!");
+            TicketHolder testTH = new TicketHolder("aiSAAC", 17, 1);
+
+            testTH.SetAge(12);
+
+            List<int> s = new List<int>() {1,0};
+            List<int> sQ = new List<int>() {12,1};
+
+            testTH.AddSnacks(s,sQ);
+            
+            List<int> d = new List<int>() {0,2};
+            List<int> dQ = new List<int>() {1,1};
+
+            testTH.AddDrinks(d,dQ);
+
+            Console.WriteLine($"{testTH.GenerateReciept()}");
+            Console.WriteLine($"{testTH.CalculateTotalCost(snackPrices,drinkPrices)}");
+        }
+    }
+}
---
+++ TicketHolder.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFundraiserV1
+{
+    class TicketHolder
+    {
+        //attributes or fields
+        private string name;
+        private int age;
+        private int numberTickets;
+        private bool credit;
+        // stores the indexes of the snacks that have been ordered
+        private List<int> snackOrder = new List<int>();
+        private List<int> snackQuantity = new List<int>();
+        // stores the indexes of the drinks that have been ordered
+        private List<int> drinkOrder = new List<int>();
+        private List<int> drinkQuantity = new List<int>();
+
+        //constructor - cronstructs an object of this class
+        public TicketHolder(string name, int age, int numberTickets)
+        {
+            this.name = name;
+            this.age = age;
+            this.numberTickets = numberTickets;
+        }
+
+        //returns the value in the private age variable
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        //Sets the value of the private age variable
+        public void SetAge(int newAge)
+        {
+            this.age = newAge;
+        }
+
+        public void SetCredit(bool newPaymentType)
+        {
+            credit = newPaymentType;
+        }
+
+        //add snacks and quantities to the snacksOrder list and snacksQuantities Lists
+        public void AddSnacks(List<int> snacks, List<int> quantities)
+        {
+            snackOrder = snacks;
+            snackQuantity = quantities;
+        }
+
+        //add drinks and quantities to the drinksOrder list and drinksQuantities Lists
+        public void AddDrinks(List<int> drinks, List<int> quantities)
+        {
+            drinkOrder = drinks;
+            drinkQuantity = quantities;
+        }
+
+        // returns string stating whether the ticket holder is paying cash or credit
+        private string PaymentType()
+        {
+            string paymentType = "credit";
+            if (credit == false)
+            {
+                paymentType = "cash";
+            }
+            return paymentType;
+        }
+
+        //returns the total cost of the purchased items
+        public float CalculateTotalCost(List<float> sPrices, List<float> dPrices, float ticketPrice)
+        {
+            float totalCost = 0.0f;
+
+            //loop through snack order and calculate the cost for each snack
+            for (int snackIndex = 0; snackIndex < snackOrder.Count; snackIndex++)
+            {
+                float snackPrice = sPrices[snackOrder[snackIndex]];
+
+                //add the cost of each snack to totalCost
+                totalCost += snackPrice * snackQuantity[snackIndex];
+            }
+
+            //loop through drink order and calculate the cost for each drink
+            for (int drinkIndex = 0; drinkIndex < drinkOrder.Count; drinkIndex++)
+            {
+                float drinkPrice = dPrices[drinkOrder[drinkIndex]];
+
+                //add the cost of each drink to totalCost
+                totalCost += drinkPrice * drinkQuantity[drinkIndex];
+            }
+
+            totalCost += CalculateTicketCost(ticketPrice);
+
+            return totalCost;
+        }
+
+        //calculate ticket cost
+        private float CalculateTicketCost(float ticketPrice)
+        {
+            return numberTickets * ticketPrice;
+        }
+
+        private string SnackDrinkSummary(List<float> sPrices, List<float> dPrices, List <string> sAvailable, List<string> dAvailable)
+        {
+            string summary = "Snacks and Drinks:\n";
+
+            //loop through snack order and quanitity, snack, total item cost to summary
+            for (int snackIndex = 0; snackIndex < snackOrder.Count; snackIndex++)
+            {
+                summary += snackQuantity[snackIndex] + " x " + sAvailable[snackOrder[snackIndex]] + "\t$" + (snackQuantity[snackIndex] * sPrices[snackOrder[snackIndex]]) + "\n";
+            }
+
+            //loop through drink order and quanitity, drink, total item cost to summary
+            for (int drinkIndex = 0; drinkIndex < drinkOrder.Count; drinkIndex++)
+            {
+                summary += drinkQuantity[drinkIndex] + " x " + sAvailable[drinkOrder[drinkIndex]] + "\t$" + (drinkQuantity[drinkIndex] * sPrices[drinkOrder[drinkIndex]]) + "\n";
+            }
+
+            return summary;
+        }
+
+        // checks if a surcharge is required
+        private bool GetSurcharge()
+        {
+            return credit;
+        }
+
+        //return string displaying surcharge cost
+        private string SurchargeSummary(List<float> sPrices, List<float> dPrices, float ticketPrice)
+        {
+            string summary = "";
+
+            if (credit)
+            {
+                summary += "Surcharge\t$" + CalculateSurcharge(sPrices, dPrices, ticketPrice);
+            }
+
+            return summary;
+        }
+
+        //returns surcharge amount
+        private float CalculateSurcharge(List<float> sPrices, List<float> dPrices, float ticketPrice)
+        {
+            // calculates surcharge
+            float surcharge = CalculateTotalCost(sPrices, dPrices, ticketPrice) * 0.2f;
+
+            return surcharge;
+        }
+
+        //calculate total amount to be paid
+        private float CalculateTotalPayment(List<float> sPrices, List<float> dPrices, float ticketPrice)
+        {
+
+            float totalPayment = CalculateTotalCost(sPrices, dPrices, ticketPrice);
+
+            if (credit)
+            {
+                totalPayment += CalculateSurcharge(sPrices, dPrices, ticketPrice);
+            }
+
+            return totalPayment;
+        }
+
+        //return a summary of the total amount to be paid
+        private string TotalPaymentSummary(List<float> sPrices, List<float> dPrices, float ticketPrice)
+        {
+            return "Total\t$" + CalculateTotalPayment(sPrices, dPrices, ticketPrice);
+        }
+
+        //returns a string diaplaying the reciept for the puchased items
+        public string GenerateReciept()
+        {
+
+
+            return $"{PaymentType()}";
+        }
+
+        // returns a string collating all the values stored in the private variables
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
